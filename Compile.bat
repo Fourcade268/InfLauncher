72a1440ecde43e600e414cfbc6f9f2dc53056b1d
Fourcade268 <88975898+Fourcade268@users.noreply.github.com>
2026-05-06 14:30:34 +0300

    chore: strict whitelist - source code removed from repository

--- Compile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Influence Launcher Compilation Tool
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "Compile.ps1"
-if %errorlevel% neq 0 (
-    echo.
-    echo [!] Произошла ошибка при выполнении скрипта.
-    pause
-)
--- CompileDeploy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Influence Launcher Release Tool
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "CompileDeploy.ps1"
-if %errorlevel% neq 0 (
-    echo.
-    echo [!] Произошла ошибка при выполнении скрипта.
-    pause
-)
